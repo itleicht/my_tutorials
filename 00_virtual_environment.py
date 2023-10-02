a72b71a8c63f906e728bfb18b693fa63e26db8c7
itleicht <descartesbs@web.de>
2023-10-02 10:29:44 +0200

    Pandas aktualisiert

---
+++ 00_virtual_environment.py
@@ -0,0 +1,12 @@
+"""
+    Anleitung zur Erstellung einer virtuellen Umgebung in Python:
+
+    1. Gib im Terminal (Terminalprompt auf aktuelles Arbeitsverzeichnis) "python3 -m venv venv" ein
+        (das zweite venv steht für den Namen des Ordners der virtuellen Umgebung)
+    2. Jetzt wurde Python in den Ordner kopiert (venv)
+    3. Aktiviere die virtuelle Umgebung mit "source venv/bin/activate"
+    4. Der Prompt des Terminals zeigt jetzt auf den virtuellen Ordner
+    5. Wähle als Python Interpreter die neue Version aus mit "Anzeigen/Befehlspalette/Python: Interpreter auswählen
+    6. Wähle "Auf Arbeitsbereichsebene auswählen" aus
+    7. Wähle die Version im Ordner venv aus
+"""
